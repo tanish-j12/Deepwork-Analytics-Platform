6fd3f8feec2ec706fb11c4db75e52fe7714ca221
tanish-j12 <tanishjindal48@gmail.com>
2026-06-10 15:29:39 +0530

    Update sessionRoutes.js

--- backend/routes/sessionRoutes.js
+++ backend/routes/sessionRoutes.js
@@ -6,5 +6,6 @@ router.post('/start', sessionController.startSession);
 router.post('/end', sessionController.endSession);
 router.get('/stats/:s_id', sessionController.getSessionStats);
 router.get('/active/:u_id', sessionController.getActiveSession);
+router.get('/topics', sessionController.getStudyTopics);
 
 module.exports = router;
